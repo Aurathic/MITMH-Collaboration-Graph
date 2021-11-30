a9bf867237e9b5941717b71e222250ff2dcea07b
Adam <39442291+Aurathic@users.noreply.github.com>
2021-11-30 12:54:59 -0500

    Copy over initial code

---
+++ katz_number.py
@@ -0,0 +1,31 @@
+import urllib3
+from bs4 import BeautifulSoup
+import networkx as nx
+from itertools import combinations 
+import matplotlib.pyplot as plt
+
+http = urllib3.PoolManager()
+main_page = http.request('GET', 'https://devjoe.appspot.com/huntindex/index/puzzles.html')
+soup = BeautifulSoup(main_page.data, 'html.parser')
+links = soup.find_all('a')[2:-1]
+
+g = nx.Graph()
+fh = open("katz_number_graph.txt",'wb')
+
+for i in links:
+    puzzle_url = f"https://devjoe.appspot.com/huntindex{i['href'][2:]}"
+    puzzle_page = http.request('GET', puzzle_url)
+    soup_puzzle = BeautifulSoup(puzzle_page.data, 'html.parser') 
+    lines = soup_puzzle.find_all('p')
+    for poss_line in lines:
+        author_line = poss_line.text
+        if author_line[:6] == "Author":
+            authors = author_line.split(":")[1].split(",")
+            for (author1, author2) in combinations(authors, 2):
+                g.add_edge(author1[1:], author2[1:])
+                print(f"{i['href']}:{author1}{author2}")
+            break
+print(g.edges())
+
+nx.write_edgelist(g, fh, delimiter=",")
+fh.close()
---
+++ katz_number_2.py
@@ -0,0 +1,18 @@
+import networkx as nx
+from itertools import combinations 
+import matplotlib.pyplot as plt
+
+fh2 = open("katz_number_graph.txt",'rb')
+g2 = nx.read_edgelist(fh2,delimiter=",")
+fh2.close()
+fig = plt.figure(num=None, figsize=(120, 80), dpi=100)
+#layout = nx.spring_layout(g2,k=0.5,iterations=100)
+layout = nx.spectral_layout(g2)
+nx.draw_networkx_edges(g2, pos=layout, width=1, edge_color=(0.33, 0.33, 0.33))
+nx.draw_networkx_nodes(g2, pos=layout, node_size=10)
+nx.draw_networkx_labels(g2, pos=layout, font_size=8)
+# plt.show()
+# fig.set_size_inches(18.5, 10.5)
+fig.savefig('mit_author_collab_graph2.svg')
+# print(g2.edges())
+# fig.savefig('plot.png')
