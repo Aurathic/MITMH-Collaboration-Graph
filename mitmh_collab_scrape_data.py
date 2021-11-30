4fa70ec17e452186ba72a5e5fecc831060c2061a
Adam <39442291+Aurathic@users.noreply.github.com>
2021-11-30 13:36:02 -0500

    Also extract puzzle and hunt name

--- mitmh_collab_scrape_data.py
+++ mitmh_collab_scrape_data.py
@@ -10,21 +10,26 @@ soup = BeautifulSoup(main_page.data, 'html.parser')
 links = soup.find_all('a')[2:-1]
 
 g = nx.Graph()
-fh = open("mitmh_collab_edges.txt",'wb')
+fh = open("mitmh_collab_edges2.txt",'wb')
 
 for i in links:
     puzzle_url = f"https://devjoe.appspot.com/huntindex{i['href'][2:]}"
     puzzle_page = http.request('GET', puzzle_url)
     soup_puzzle = BeautifulSoup(puzzle_page.data, 'html.parser') 
+    puzzle_name = soup_puzzle.find('h2').text
     lines = soup_puzzle.find_all('p')
     for poss_line in lines:
-        author_line = poss_line.text
-        if author_line[:6] == "Author":
-            authors = author_line.split(":")[1].split(",")
+        poss_line_text = poss_line.text
+        print(poss_line_text)
+        if poss_line_text[:4] == "Hunt":
+            hunt_name = poss_line_text.split(":")[1][1:-1]
+        elif poss_line_text[:6] == "Author":
+            authors = poss_line_text.split(":")[1].split(",")
             for (author1, author2) in combinations(authors, 2):
-                g.add_edge(author1[1:], author2[1:])
+                g.add_edge(author1[1:], author2[1:], puzzle_name=puzzle_name, hunt_name=hunt_name)
                 print(f"{i['href']}:{author1}{author2}")
             break
+        # TODO Extract keywords
 print(g.edges())
 
 nx.write_edgelist(g, fh, delimiter=",")
